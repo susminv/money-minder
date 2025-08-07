dddb3482dcff88d2d470a51a8b0dc529a19323e6
unknown <susmin@inapp.com>
2025-08-07 17:28:07 +0530

    FE0004 : Set up folder structure

--- money-minder/src/app/app.component.ts
+++ money-minder/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [LayoutComponent],
+  template: '<app-layout></app-layout>',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
